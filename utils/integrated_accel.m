7f9efb7abc9f09ac2d3a58f8586b829e89cba578
Bruno <brunolnetto@gmail.com>
2020-03-04 14:37:23 -0300

    Minus out

--- utils/integrated_accel.m
+++ utils/integrated_accel.m
@@ -12,7 +12,7 @@ function z_bar = integrated_accel(m_1, xhat_1, delta_xhat_1, ...
     
     % Calculation of mean for stochastic function
     e_z = exp(-nu*oracle(xhat_1));
-    z_bar = -(1/m_1)*(lambda_z*e_z*delta_xhat_1 + z_bar_1*m_2);
+    z_bar = (1/m_1)*(lambda_z*e_z*delta_xhat_1 + z_bar_1*m_2);
     
     m_2 = m_1;
     z_bar_1 = z_bar;
