93f630c697e3636091f25c37c37e9d929a9fa1da
Bruno <brunolnetto@gmail.com>
2020-12-26 19:11:15 -0300

    Temporary delete files

--- utils/non_accel.m
+++
@@ -1,3 +0,0 @@
-function z_bar = non_accel(m_1, xhat_1, delta_xhat_1)
-    z_bar = zeros(size(xhat_1));
-end
--- utils/pait_accel.m
+++
@@ -1,3 +0,0 @@
-function z_bar = pait_accel(m_1, xhat_1, delta_xhat_1, zeta)
-    z_bar = zeta*delta_xhat_1;
-end
